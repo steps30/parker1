536747c37d0a5fbdbfeba277704232c966334de1
steps30 <stepskate3021@gmail.com>
2025-04-11 15:47:25 +0530

    Maven commit

---
+++ src/main/java/com/parkersteps_maven/parker/ParkerApplication.java
@@ -0,0 +1,13 @@
+package com.parkersteps_maven.parker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkersteps_maven/parker/ParkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkersteps_maven.parker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
